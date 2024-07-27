6dd5a38d500c90da151b5787e26a988a8e62f138
Rafael Santos <rafa.05125@gmail.com>
2024-07-27 15:52:37 -0300

    Timer style

---
+++ BlazingCountdown/BlazingCountdown.Client/NumberExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazingCountdown.Client
+{
+    public static class NumberExtensions
+    {
+        public static string PadLeftExtension(this int number)
+            => number.ToString().PadLeft(2, '0');
+    }
+}
